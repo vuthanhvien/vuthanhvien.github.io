4a152f59c55e89bd7d635e0f9286264c1f7af010
vien <vuthanhvien742@gmail.com>
2019-02-18 22:06:11 +0700

    update

---
+++ vien/src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="header container">
+    <div class="logo">
+      <img src="logo.png">
+    </div>
+    <div class="menu">
+      <ul>
+        <li><a>Home</a></li>
+        <li><a>Programing</a></li>
+        <li><a>Design</a></li>
+        <li><a>Who am I?</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+    </div>
+    <div class="search">
+      <i class="fa fa-search"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  padding-top: 25px;
+  display: flex;
+  border-bottom: 1px solid #ccc;
+  .logo {
+    padding: 5px;
+    img {
+      height: 40px;
+    }
+  }
+  & > div {
+    display: flex;
+  }
+  .menu {
+    flex: auto;
+    margin-left: 25px;
+  }
+  .search {
+    padding: 15px;
+    cursor: pointer;
+    height: 45px;
+    width: 45px;
+    border-radius: 25px;
+    background: #f4f4f4;
+    &:hover{
+      background: #ca3a5c;
+      color: white;
+    }
+  }
+  ul {
+    margin: 0;
+    list-style: none;
+    li {
+      display: inline-block;
+      padding: 15px 25px;
+      a{
+        font-weight: bold;
+        cursor: pointer;
+        &:hover{
+          color: #ca3a5c;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ vien/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home ">
+    <Header/>
+    <Intro/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Intro from "@/components/Intro.vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "home",
+  components: {
+    Intro,
+    Header,
+    Footer
+  }
+};
+</script>
+<style lang="scss">
+body {
+  font-family: "Cabin", sans-serif!important;
+}
+p,a,span{
+  font-size: 14px;
+}
+</style>
+
