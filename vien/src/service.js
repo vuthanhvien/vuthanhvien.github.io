97c6dc276018a8bc94f56acfe5e5cea2f8cbd762
Thằng Ku Bờm <vuthanhvien742@gmail.com>
2019-02-20 14:10:21 +0700

    update

--- vien/src/service.js
+++ vien/src/service.js
@@ -2,23 +2,21 @@ import firebase from 'firebase/app'
 import 'firebase/firestore';
 
 if (!firebase.apps.length) {
-firebase.initializeApp({
-    projectId: 'vienvu-7e64f',
-    databaseURL: 'fds afd asfd '
-})
+    firebase.initializeApp({
+        projectId: 'vienvu-7e64f',
+        databaseURL: ''
+    })
 }
 
 
 const db = firebase.firestore();
 
-export const getPosts = () => {
-    const posts = db.collection("post").orderBy("createdAt", "desc").get();
-
+export const getPosts = (page = 1) => {
+    const posts = db.collection("post").orderBy("createdAt", "desc").limit(20).startAfter(20 * (page - 1)).get();
     return posts;
 }
 
 export const getPost = (id) => {
     const post = db.collection("post").doc(id).get();
-
     return post;
 }
--- vien/vue.config.js
+++ vien/vue.config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    outputDir:  "./../dist"
+    outputDir:  "./../"
 }
