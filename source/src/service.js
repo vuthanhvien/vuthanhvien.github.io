e43199ec9366659a0a798ed88b190a91b021639b
Vien Vu <vien.vu@systum.com>
2019-05-30 16:58:38 +0700

    new

--- source/src/service.js
+++ source/src/service.js
@@ -41,7 +41,7 @@ export const getPost = async (id) => {
     return data;
 }
 export const savePost = async (data) => {
-    data.tags = data.tag.join(',');
+    data.tags = data.tags ? data.tags.join(',') : '';
     await firebase.collection('post').doc(data.id).set(data);
     return data;
 
